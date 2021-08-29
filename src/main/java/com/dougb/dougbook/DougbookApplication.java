55d691b6179b9649a473145441fe2b2885d5d14a
Doug Baek <baekdoug@gmail.com>
2021-08-28 20:16:01 -0500

    -initial commit with initialized spring project

---
+++ src/main/java/com/dougb/dougbook/DougbookApplication.java
@@ -0,0 +1,13 @@
+package com.dougb.dougbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DougbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DougbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dougb/dougbook/DougbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougb.dougbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DougbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
